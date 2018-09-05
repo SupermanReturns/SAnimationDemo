6fa55c53045327fff3921341c80ead36f5716a1c
Superman <703353105@qq.com>
2018-09-05 22:34:43 +0800

    修改代码

--- SAnimationDemo/ViewController.m
+++ SAnimationDemo/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic)  UIView *firstView;
+@property (strong, nonatomic)  UIView *secondView;
+@property (strong, nonatomic)  UIView *threeView;
+@property (strong, nonatomic)  UIView *fourView;
 
 @end
 
@@ -17,9 +21,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self initUI];
+    [self restartAnimation];
 }
-
-
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+}
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    
+}
+-(void)initUI{
+    
+}
+-(void)restartAnimation{
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
